f1d50e14087501bc76a7987c0ecfdbfc4630ed2a
XTxiaoting14332 <HT30332@126.com>
2024-07-13 17:48:53 +0800

    美化输出

--- api/nbgui/proxy/plugins.py
+++ api/nbgui/proxy/plugins.py
@@ -9,8 +9,8 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'application/json; charset=utf-8')
         self.end_headers()
         response = httpx.get('https://registry.nonebot.dev/plugins.json')
-        res_raw = response.text
-        res = json.dumps(str(res_raw), ensure_ascii=False, indent=4)
+        res_raw = response.json
+        res = json.dumps(res_raw, ensure_ascii=False, indent=4)
         self.wfile.write(res.encode('utf-8'))
 
 
