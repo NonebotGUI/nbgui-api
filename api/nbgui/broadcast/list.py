9257b25b23d893d9a18538e4e44f3344c14728a2
XTxiaoting14332 <HT30332@126.com>
2024-07-13 13:38:23 +0800

    Test

--- api/nbgui/broadcast/list.py
+++ api/nbgui/broadcast/list.py
@@ -1,18 +1,22 @@
 from http.server import BaseHTTPRequestHandler
 import os
 import json
-from frontmatter import Frontmatter
+from datetime import datetime
+
+def get_list():
+    json_list = []
+    for id, file_name in enumerate(sorted(os.listdir('data'),reverse=True)):
+        if file_name.endswith('.md'):
+            path = os.path.join('data', file_name)
+            time = datetime.fromtimestamp(os.path.getmtime(path)).strftime('%Y-%m-%d %H:%M:%S')
+            json_list.append({"name": file_name, "time": time, "id": id})
+    return json_list
+
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        json_list = []
-        for id,file_name in enumerate(sorted(os.listdir('data'),reverse=True)):
-            if file_name.endswith('.md'):
-                path = os.path.join('data', file_name)
-                md = Frontmatter.read_file(path)
-                time = md['attributes']['time']
-                id = md['attributes']['id']
-                json_list.append({"name": file_name, "time": time, "id": id})
+        json_list = get_list()
         res = json.dumps(json_list, ensure_ascii=False, indent=4)
         self.send_response(200)
         self.send_header('Content-Type', 'application/json; charset=utf-8')
