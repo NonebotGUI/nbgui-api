310d2d57ce1bf81247e43ae7c040d4e99ce77551
XTxiaoting14332 <HT30332@126.com>
2024-07-13 17:32:49 +0800

    新增对NoneBot商店列表的GBK化

---
+++ api/nbgui/proxy/drivers/index.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import httpx
+
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json; charset=utf-8')
+        self.end_headers()
+        response = httpx.get('https://registry.nonebot.dev/drivers.json')
+        res_raw = response.text
+        res = json.dumps(res_raw,ensure_ascii=False,indent=4)
+        self.wfile.write(res.encode('utf-8'))
+
