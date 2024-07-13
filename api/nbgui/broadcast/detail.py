f1d40cb2af3f51b79dbd50d11947068e2fa9687c
XTxiaoting14332 <HT30332@126.com>
2024-07-13 15:22:28 +0800

    Fox

--- api/nbgui/broadcast/detail.py
+++ api/nbgui/broadcast/detail.py
@@ -60,5 +60,6 @@ class handler(BaseHTTPRequestHandler):
         query = parse.urlparse(self.path).query
         query_components = parse.parse_qs(query)
         id = query_components.get('id', [None])[0]
-        res = get_detail(int(id))
+        id = int(id)
+        res = get_detail(id)
         self.wfile.write(res.encode('utf-8'))
