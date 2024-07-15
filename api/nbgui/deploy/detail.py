2e7f4d917502dfbfc4cad63bd0ad59f77a8f0751
XTxiaoting14332 <HT30332@126.com>
2024-07-15 18:06:46 +0800

    fix bug

--- api/nbgui/deploy/detail.py
+++ api/nbgui/deploy/detail.py
@@ -18,11 +18,12 @@ def get_list():
 
 
 
+
 def get_detail(id):
     if id is not None:
         try:
             id = int(id)
-            list = json.loads(get_list())
+            list = get_list()
             name = next((item['name'] for item in list if item['id'] == id), None)
             if name:
                 with open(f'api/nbgui/deploy/template/{name}', 'r', encoding='utf-8') as file:
@@ -52,5 +53,4 @@ class handler(BaseHTTPRequestHandler):
         query_components = parse.parse_qs(query)
         id = query_components.get('id', [None])[0]
         res = get_detail(id)
-        res = json.loads(res)
-        self.wfile.write(res)
+        self.wfile.write(res.encode('utf-8'))
