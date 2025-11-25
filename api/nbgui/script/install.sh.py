bbe8923e2f7e47a0f6d5335d9c50b1bb032c177a
XTxiaoting14332 <HT30332@126.com>
2025-11-25 09:11:59 +0800

    Fix bug

--- api/nbgui/script/install.sh.py
+++ api/nbgui/script/install.sh.py
@@ -4,7 +4,7 @@ import os
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):   
         try:
-            with open('api/nbgui/script/install' "r", encoding="utf-8") as file:
+            with open('api/nbgui/script/install', "r", encoding="utf-8") as file:
                 script_content = file.read()
             self.send_response(200)
             self.send_header('Content-Type', 'text/plain; charset=utf-8')
