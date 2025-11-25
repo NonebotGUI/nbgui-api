1e744a5ed0fa65d4406e255998a05850432fdc1c
XTxiaoting14332 <HT30332@126.com>
2025-11-25 09:01:23 +0800

    新增安装脚本

---
+++ api/nbgui/script/install.sh.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import os
+
+class InstallScriptHandler(BaseHTTPRequestHandler):
+    def do_GET(self):   
+        try:
+            with open('api/nbgui/script/install' "r", encoding="utf-8") as file:
+                script_content = file.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(script_content.encode("utf-8"))
+        
+
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-Type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(f"Internal Server Error: {str(e)}".encode("utf-8"))
